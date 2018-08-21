3f052e3ce0481297a20e9f108bfb7f46a2f331b8
Waqar Ali <waqar@adeven.com>
2018-08-21 21:55:03 +0200

    Add react redux todo example

--- src/app/common/common.js
+++ src/app/common/common.js
@@ -1,5 +1,9 @@
 import angular from 'angular';
+import { react2angular } from 'react2angular'
+
+import reactApp from '../index.js';
 
 export default angular
   .module('esmTest.common', [])
+  .component('react', react2angular(reactApp, []))
   .name;
